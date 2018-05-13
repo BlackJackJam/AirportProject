eed4060936f7bcd541c5e0d50a45b0404e026b77
BlackJackJam <nfskicker@foxmail.com>
2018-05-13 17:18:49 +0800

    Complete Random()(possion and uniform)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cmath>
+#include <random>//Template of random
+#include <time.h>//Use the clock to provide random numbers
 
 using namespace std;
 enum Error_code{success,underflow,overflow};
@@ -24,19 +27,49 @@ extern int maxqueue
 
 class Random
 {
- public:
+public:
      int possion(double rate) const;
      double uniform() const;
+private:
+     int fac(int n) const;
 };
 
-int Random::possion(double rate)//使用inverse CDF方法（cumulative distribution function）将possion分布的反函数求出，通过均匀分布随机数，得出possion分布随机数
-{
 
+int Random::possion(double rate) const//使用inverse CDF方法（cumulative distribution function）将possion分布的反函数求出，通过均匀分布随机数，得出possion分布随机数
+//只要生成一个0到1之间的随机数，然后看泊松分布的前几项和刚好大于这个随机数就行了。(所谓的inverse CDF方法)
+{
+    double u,p1;
+    int i=0,s;
+    u=uniform();
+    p1=0;
+    do
+    {
+        s=fac(i);//阶乘
+        p1+=(exp(-rate)*rate^i)/s;//加和泊松分布
+        i++;
+    }while(pi<u)
+    return i;
 }
 
-double Random::uniform()//0~1之间均匀分布随机函数，均匀随机生成0~1之间的数（作为possion分布函数的概率P(x=k)，反求对应的k数值），参考C11浮点数均匀分布模板
+double Random::uniform() const//0~1之间均匀分布随机函数，均匀随机生成0~1之间的数（作为possion分布函数的概率P(x=k)，反求对应的k数值），参考C11浮点数均匀分布模板
 {
+    std::default_random_engine random(time(NULL));
+    std::uniform_real_distribution<double> dis2(0.0,1.0);
+    return dis2(random);
+}
 
+int Random::fac(int n) const
+{
+    if(n=0) return 1;
+    else
+    {
+        int k,n1=1;
+    for (k=1;k<=n;k++)
+    {
+        n1*=k;
+    }
+    return n1;
+    }
 }
 
 template<typename T> class Extended_Queue
